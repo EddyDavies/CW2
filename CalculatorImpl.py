e38424f6c55886649a2875729314be96a643a526
Eddy <git@eddydavies.com>
2020-11-08 12:46:36 +0000

    Fully Working with all Examples

--- CalculatorImpl.py
+++ CalculatorImpl.py
@@ -8,8 +8,9 @@ def evaluate_expr(parsed_expr):
        That is, if the JSON expresses '1+1', this function returns '2'."""
 
     op = {"actions": None}
-    total = getDeepest(parsed_expr, op, 0)
+    total = getDeepest(parsed_expr, op)
     
+    print(parsed_expr["description"])
     return total
 def load_json_expr(json_path):
     """This function takes a file path to a JSON file  represened
@@ -22,54 +23,46 @@ def load_json_expr(json_path):
     
     return data
 
-def getDeepest(data, op, total):
+def getDeepest(data, op):
+    total = []
+
     if op["actions"] is None:
-        print("Action is None")
+        # print("Action is None")
         op = getOperation(data)
         if op["calculate"]:
-            print("Calculate from None")
+            # print("Calculate from None")
             total = performOperation(data, op, total)
-
-    print()
-    print("op")
-    print("        ", op)
-    print("data")
-    print("        ",data)
-    print()
+            return total
+            
 
     for action in op["actions"]:
         for i in range(action[1]):
+            total.append(None)
+
+        
+        for i in range(action[1]):
             current_data = data[action[0]][i]
             current_op = getOperation(current_data)
+            # print()
+            # print("current_op")
+            # print("        ", current_op)
+            # print("current_data")
+            # print("        ",current_data)
+            # print()
             if current_op["calculate"] and not current_op["tier_up"]:
-                # print()
-                # print("current_op")
-                # print("        ", current_op)
-                # print("current_data")
-                # print("        ",current_data)
-                # print()
                 # print("Perform ", action[0], " calculation on current")
-                total = performOperation(current_data, current_op, total)
+                total[i] = performOperation(current_data, current_op, total)
             elif current_op["tier_up"]:
-                # print()
-                # print("op")
-                # print("        ", op)
-                # print("data")
-                # print("        ",data)
-                # print()
                 # print("Perform ", action[0], " calculation on layer above.")
-                total = performOperation(data, op, total)
+                total[i] = performOperation(data, op, total)
             else:
-                # print()
-                # print("current_op")
-                # print("        ", current_op)
-                # print("current_data")
-                # print("        ",current_data)
-                # print()
                 # print("Another layer Nested inside the ", action[0])
-                total = getDeepest(current_data, op, total)
+                total[i] = getDeepest(current_data, current_op)
+            
+            if total[-1] is not None:
+                # print("Perform ", action[0], " calculation of totals.")
+                total = performOperation(data, op, total)
 
-            print("Running Total = ", total)
 
     return total
 
@@ -136,18 +129,17 @@ def getOperation(data):
     return op
 
 def performOperation(data, op, total):
-    
+
     calculated = 0
-    print("Data         " , data)
+    first = None
 
     for action in op["actions"]:
-        first = None
 
         for i in range(action[1]):
             try:
                 value = data[action[0]][i]["int"]
             except: 
-                value = total
+                value = total[i]
 
             if action[0] is "plus":
                 calculated += value 
@@ -160,43 +152,15 @@ def performOperation(data, op, total):
                     calculated = first - value
                 first = None
 
-    print("                       ",op)
-    print()
-    print("                       ",data)
-    print()
-    print(calculated)
+        # print("Calculated as = ", calculated)
+
 
     return calculated
 
-def getDeepest(data, op, total):
-    if op["actions"] is None:
-        op = getOperation(data)
-
-    for action in op["actions"]:
-        for i in range(action[1]):
-            current_data = data[action[0]][i]
-            current_op = getOperation(current_data)
-            # print(action[0])
-            # print("                       ",op)
-            # print()
-            # print("                       ",data)
-            # print()
-            if current_op["calculate"]:
-                print(action[0], " calculate it")
-                total = performOperation(data, current_op, total)
-                break
-            else:
-                print(action[0]," go deeper")
-                total = getDeepest(current_data, current_op, total)
-
-    
-    return total
 
 # You do not need to touch anything below this line. To complete
 # the assignment you need to replace the keyword "pass" in the above
 # two functions with code that does the appropriate work.
 if __name__=='__main__':
-    expr_file_path = "/Users/edwarddavies/Documents/UoM/_Modelling Data/CW2/expression1.json"
-
-    # expr_file_path = sys.argv[1]
+    expr_file_path = sys.argv[1]
     print(evaluate_expr(load_json_expr(expr_file_path)))
